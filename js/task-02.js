f6fe244cbfcbf23fc01cc32ecde7e37e3fe7e785
Taras Bondar <strelezzzz@gmail.com>
2022-04-27 00:52:41 +0300

    task02

--- js/task-02.js
+++ js/task-02.js
@@ -6,9 +6,28 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+//     Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+//     Додасть назву інгредієнта як його текстовий вміст.
+//     Додасть елементу клас item.
+//     Після чого, вставить усі <li> за одну операцію у список ul.ingredients.
+
 // elem.classList.add(cls) - додає клас cls до списку класів елемента.
 // document.createElement(tagName);
 // const heading = document.createElement("h1");
 // heading.textContent = "This is a heading";
 
-document.createElement("li");
+
+const items = ingredients.map( name => {
+  const ingredient = document.createElement("li");
+  ingredient.classList.add('item');
+  ingredient.textContent = name;
+  // console.log(ingredient);
+  return ingredient;
+})
+
+// console.log(items);
+const ingredientsEl = document.querySelector('#ingredients');
+ingredientsEl.append(...items);
