490a7493338a2362a41e61a705328d7f0af1f11d
Taras Bondar <strelezzzz@gmail.com>
2022-04-29 16:53:48 +0300

    Update task 04

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,7 @@
 // Завдання 4
 
-// Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
+// Лічильник складається зі спану і кнопок,
+//  які по кліку повинні збільшувати і зменшувати його значення на одиницю.
 
 // <div id="counter">
 //   <button type="button" data-action="decrement">-1</button>
@@ -10,6 +11,19 @@
 
 //   1)  Створи змінну counterValue, в якій буде зберігатися поточне значення
 //  лічильника та ініціалізуй її значенням 0.
-//    2) Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй 
+//    2) Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй
 // значення лічильника.
 //    3) Оновлюй інтерфейс новим значенням змінної counterValue.
+
+let counterValue = 0;
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+// console.log(decrementBtn);
+const incrementBtn = document.querySelector('[data-action="increment"]');
+// console.log(incrementBtn);
+
+decrementBtn.addEventListener('click',onTargetButtonClick);
+
+function onTargetButtonClick(){
+    console.log('Click');
+}
